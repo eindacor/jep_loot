9cbb05969a285e76ab0fc18ed79ad505cc32d71a
Joe Pollack <jpollack320@gmail.com>
2013-09-13 17:40:29 -0400

    README added

--- jeploot.cpp
+++ jeploot.cpp
@@ -1,20 +1,14 @@
-//#include <vector>
 #include <time.h>
 #include <cstdarg>
 #include <stdlib.h>
 
-//using std::vector;
-
 namespace jep
 {
-
-	//Initializes random seed.
 	void init()
 	{
 		srand(time(NULL));
 	}
 
-	//Returns true based on percent chance in decimal.
 	bool booRoll(double chance)
 	{
 		if (chance >= 1)
@@ -26,7 +20,6 @@ namespace jep
 		return (rand() % 1000 < chance * 1000);
 	}
 
-	//Returns a number between a range of two integers.
 	int intRoll(int n1, int n2)
 	{
 		if (n1==n2)
@@ -38,48 +31,6 @@ namespace jep
 		if (n2<n1)
 			return n2 + (rand() % (1+n1-n2));
 	}
-
-	//Takes a number of values (x), and returns a number from 0 to (x-1), with larger values having a proportionately
-	//larger chance of rolling. Passing a vector containing ints 10, 20, and 70 would have a 10:100 chance to roll a 0, 
-	//a 20:100 chance to roll a 1, and a 70:100 chance to roll a 2.
-	/*
-	int catRoll(vector <int> &chances)
-	{
-		int seedRange=0;
-
-		//combines all numbers from chances
-		for (int i=0; i<chances.size(); i++)
-		{
-			seedRange += chances.at(i);
-		}
-
-		//rolls random number between 1 and total from above
-		int seed = jep::intRoll(1, seedRange);
-
-		//cout << "jep::catRoll seed = " << seed << endl;
-
-		vector <int> rangeCheck;
-
-		//generates seed checklist
-		for (int i=0; i<chances.size(); i++)
-		{
-			int numberToPush=0;
-
-			for(int c=0; c<i+1; c++)
-			{
-				numberToPush += chances.at(c);
-			}
-
-			rangeCheck.push_back(numberToPush);
-		}
-
-		for (int i=0; i<rangeCheck.size(); i++)
-		{
-			if (seed <= rangeCheck.at(i))
-				return i;
-		}
-	}
-	*/
 
 	int catRoll(int n, ...)
 	{
---
+++ jeplootREADME.txt
@@ -0,0 +1,25 @@
+jeploot library README
+
+
+Function				Description
+----------------------------------------------------------------------------------------------------
+jep::init()				Initializes jeploot file. 
+
+
+jep::booRoll(double d)			Returns true based on percent chance passed (in decimal
+					form). Values greater than or equal to 1 will always return
+					true, values less than or equal to 0 will always return
+					false.
+
+
+jep::intRoll(int n1, int n2)		Returns a random int, from a range between the two integers
+					passed.
+
+
+jep::catRoll(int n, ...)		Takes n values (ints). Each value represents categories 1-n
+					respectively. Each category's chance of rolling is equal to
+					that category's ratio to the sumtotal of all values. For
+					instance, jep::catRoll(4, 10, 20, 60, 45) would have 10:135
+					odds to return 1, 20:135 odds to return 2, 60:135 odds to
+					return 3, and 45:135 odds to return 4.
+----------------------------------------------------------------------------------------------------
