110d6a27dd85b17dd3ea0f88db49a089ab843727
Joseph Pollack <jpollack320@gmail.com>
2014-11-04 21:11:01 -0500

    updated test script

--- jeploot_test.py
+++ jeploot_test.py
@@ -1,12 +1,96 @@
 import jeploot
 import random
+from enum import Enum
 
 jeploot.init()
+
+class item_type(Enum):
+    armor = 1
+    weapon = 2
+    material = 3
+    consumable = 4
+
+class item(object):
+
+    value = 0
+    original_durability = 100.0
+    durability = 100.0
+    wear = 0.0
+
+    def __init__(self, value, item_type):
+        self.value = value
+        self.item_type = item_type
+
+    def getValue(self):
+        wear = self.durability / self.original_durability
+        return float(self.value) * wear
+
+    def getDurability(self):
+        return self.durability
+
+class weapon(item):
+
+    def __init__(self, damage, value):
+        self.damage = damage
+        self.value = value
+        self.item_type = weapon
+
+    def use(self):
+        print ("Swing weapon")
+        if self.durability > 0:
+            self.durability -= 10
+
+class armor(item):
+
+    def __init__(self, strength, value):
+        self.strength = strength
+        self.value = value
+        self.item_type = armor
+
+    def use(self):
+        print ("Block attack")
+        if self.durability > 0:
+            self.durability -= 10
+
+def print_condition(item, name):
+    print ("%s durability: %d\n%s value: %d" % (name, item.getDurability(), name, item.getValue()))
+
+def legendary_item():
+    print ("Legendary item found!")
+
+def rare_item():
+    print ("Rare item found!")
+
+def common_item():
+    print ("Common item found!")
+
+# dictionary loaded with functions
+function_dict = {legendary_item: 1, rare_item: 3, common_item: 6}
+
+for i in range(10):
+    jeploot.catRoll(function_dict)()
+
+# testing parent/child classes
+axe = weapon(50, 100)
+chestplate = armor(75, 120)
+
+print_condition(axe, "Battle Axe")
+print_condition(chestplate, "Guilded Chestplate")
+axe.use()
+chestplate.use()
+print_condition(axe, "Battle Axe")
+print_condition(chestplate, "Guilded Chestplate")
+axe.use()
+chestplate.use()
+print_condition(axe, "Battle Axe")
+print_condition(chestplate, "Guilded Chestplate")
 
 chance_test = .3
 string_test = "this is a test"
 int_test = 42
 
+
+# testing jeploot functions
 hit_count = 0
 
 for i in range(1000):
@@ -23,7 +107,7 @@ list_results = [0, 0, 0, 0]
 dict_results = [0, 0, 0]
 
 for i in range(1000):
-    selection = jeploot.catRoll(list_one)
+    selection = jeploot.catRoll(list_two)
     list_results[selection] += 1
 
 for i in range(1000):
