1c0937d8e1760698d1e59fb5f1f08c3dd530b500
Joseph Pollack <jpollack320@gmail.com>
2014-12-13 17:22:04 +0000

    floating point values fixed in catRoll

--- jeploot.py
+++ jeploot.py
@@ -16,8 +16,13 @@ def booRoll(float_chance):
 
     else: return random.random() < float_chance
 
+# for list passed (x, y, z), random value returns according to the 
+# proportion of x:sum, y:sum, z:sum
+# catRoll(10, 20, 30) has a 10/60 chance of returning 0, 20/60 chance of
+# returning 1, and 30/60 chance of returning 2
+# dicts passed use the value of each element for the proportion, and return
+# a corresponding key
 def catRoll(options):
-    
     if len(options) == 0:
         raise Exception("this function does not accept empty sequences")
     
@@ -29,7 +34,11 @@ def catRoll(options):
         if specified_type == int:
             seed = random.randint(0, total)
 
-        else : seed = random.random()
+        elif specified_type == float:
+            seed = random.uniform(0, total)
+            
+        else:
+            raise Exeption("function detected invalid proportion values")
 
         current_check = specified_type(0)
         for i in range(len(options)):
